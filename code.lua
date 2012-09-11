9d368ec75eb93876c91533b02c3227c0a8f30bd6
John Cramp <john>
2012-09-10 19:46:41 -0700

    well, I think it works
    - hide completed quests
    - change font and shrink a little

---
+++ code.lua
@@ -0,0 +1,50 @@
+local frame = CreateFrame('Frame')
+
+local lastQuestIndex = -1
+
+function frame.Update(self, event, ...)
+	if ... then print(event .. " (" .. ... .. ")") else print(event) end
+
+	if event == "QUEST_WATCH_UPDATE" then
+		lastQuestIndex = ...
+		return
+	end
+	-- UNIT_QUEST_LOG_CHANGED
+	if ... ~= "player" then return end
+
+	local index = lastQuestIndex
+	print(GetQuestLink(index))
+	print(GetQuestLogTitle(index))
+	local _, _, _, _, _, _, isComplete, _, _, _, _ = GetQuestLogTitle(index)
+	print(isComplete)
+	if isComplete then RemoveQuestWatch(index) end
+end
+
+-- this guy tells us what quest is affected but fires before the completion
+-- status is updated
+frame:RegisterEvent("QUEST_WATCH_UPDATE")
+-- this provides different info but fires at the right time!
+-- XXX also fires when turning in quests but I don't think I care
+frame:RegisterEvent("UNIT_QUEST_LOG_CHANGED")
+-- Quixote uses this one w/ quest log scanning (but it requires more things I
+-- don't need)
+--frame:RegisterEvent("QUEST_LOG_UPDATE")
+frame:SetScript("OnEvent", frame.Update)
+
+
+WatchFrame:SetScale(0.9)
+
+local ABF = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\ABF.ttf"
+local SIZE = 12
+WatchFrameTitle:SetFont(ABF, SIZE)
+-- XXX I think this will need to be run (at least) when new quests are tracked
+for i = 1, 50 do
+	line = _G["WatchFrameLine"..i]
+	if line then
+		line.text:SetFont(ABF, SIZE)
+		line.dash:SetFont(ABF, SIZE)
+	else
+		break
+	end
+end
+WatchFrame_Update()
