e08b68cd644100343b76dfa6788eb61e9d1526d1
John Cramp <john>
2012-09-11 21:46:45 -0700

    notes for posterity

--- code.lua
+++ code.lua
@@ -24,7 +24,7 @@ function frame.Update(self, event, ...)
 	-- - keep track and only do it if I need a new row created
 	-- - somehow hook into the new row creation
 	-- XXX this isn't even soon enough anyway
-	self.StyleWatchFrame()
+	--self.StyleWatchFrame()
 end
 
 -- this guy tells us what quest is affected but fires before the completion
@@ -44,6 +44,8 @@ WatchFrame:SetScale(0.9)
 local ABF = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\ABF.ttf"
 local SIZE = 12
 
+-- TODO think this is how this should be done:
+--function frame:StyleWatchFrame()
 function frame.StyleWatchFrame(self)
 	print("StyleWatchFrame")
 	WatchFrameTitle:SetFont(ABF, SIZE)
@@ -59,4 +61,45 @@ function frame.StyleWatchFrame(self)
 	WatchFrame_Update()
 end
 
-frame.StyleWatchFrame()
+--frame.StyleWatchFrame()
+
+--WatchFrameLineTemplate
+-- does this variable exist? 
+--   *when* can I modify it?
+-- I think this will work! at least for new quests
+-- *if* I change this early enough I think it'll solve it everywhere
+-- I may need to remove delayed loading however (?)
+-- XXX if this is virtual does this mean that I can't reference it here?
+-- 	can I modify the template?
+-- 	  can I even *reference* it?
+-- 	or change which template is used?
+-- 	  TODO:
+--[[
+JQuest_WatchFrameLineTemplate = CreateFrame("FRAME", "JQuest_WatchFrameLineTemplate", WatchFrameLineTemplate, "WatchFrameLineTemplate")
+-- so how much will I fuck things up if I replace this at runtime?
+WatchFrame.lineCache = UIFrameCache:New("FRAME", "WatchFrameLine", WatchFrameLines, "JQuest_WatchFrameLineTemplate");
+-- TODO somehow cleanup the old cache
+--]]
+
+--[[
+local oldWFOL = WatchFrame_OnLoad
+function MyWatchFrame_OnLoad(self)
+	oldWFOL(self)
+	print("WFOL")
+end
+WatchFrame_OnLoad = MyWatchFrame_OnLoad
+]]--
+
+--WatchFrameLineTemplate.text:SetFont(ABF, SIZE)
+--WatchFrameLineTemplate.dash:SetFont(ABF, SIZE)
+-- XXX also virtual
+--WatchFontTemplate:SetFont(ABF, SIZE)
+
+local oldWFLTOL = WatchFrameLineTemplate_OnLoad
+local function JQuest_WatchFrameLineTemplate_OnLoad(self)
+  print("WatchFrameLineTemplate_OnLoad")
+  oldWFLTOL(self)
+  self.text:SetFont(ABF, SIZE)
+  self.dash:SetFont(ABF, SIZE)
+end
+WatchFrameLineTemplate_OnLoad = JQuest_WatchFrameLineTemplate_OnLoad
