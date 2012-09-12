dad79940c348a535521abca99dbaa01b4efd9c41
John Cramp <john>
2012-09-11 22:09:16 -0700

    style objective tracker title

--- code.lua
+++ code.lua
@@ -3,6 +3,8 @@ WatchFrame:SetScale(0.9)
 local ABF = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\ABF.ttf"
 local SIZE = 12
 
+WatchFrameTitle:SetFont(ABF, SIZE)
+
 -- TODO have this run early enough to get already tracked quests
 local oldWFLTOL = WatchFrameLineTemplate_OnLoad
 local function JQuest_WatchFrameLineTemplate_OnLoad(self)
