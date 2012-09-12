da27bfc860eaf6d3c41811686b99364641301d0a
John Cramp <john>
2012-09-12 14:56:37 -0700

    apply style on tracker creation

--- code.lua
+++ code.lua
@@ -1,9 +1,5 @@
-WatchFrame:SetScale(0.9)
-
 local ABF = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\ABF.ttf"
-local SIZE = 12
-
-WatchFrameTitle:SetFont(ABF, SIZE)
+local FONT_SIZE = 12
 
 -- TODO have this run early enough to get already tracked quests
 local oldWFLTOL = WatchFrameLineTemplate_OnLoad
@@ -11,7 +7,20 @@ local function JQuest_WatchFrameLineTemplate_OnLoad(self)
   --print("WatchFrameLineTemplate_OnLoad")
   oldWFLTOL(self)
   -- TODO tighten up spacing?
-  self.text:SetFont(ABF, SIZE)
-  self.dash:SetFont(ABF, SIZE)
+  self.text:SetFont(ABF, FONT_SIZE)
+  self.dash:SetFont(ABF, FONT_SIZE)
 end
 WatchFrameLineTemplate_OnLoad = JQuest_WatchFrameLineTemplate_OnLoad
+
+-- XXX this needs to be redone if the tracker is ever hidden
+-- XXX uh does this happen?
+local oldWFOL = WatchFrame_OnLoad
+local function JQuest_WatchFrame_OnLoad(self)
+  print("WatchFrame_OnLoad")
+  oldWFOL(self)
+  self:SetScale(0.9)
+  WatchFrameTitle:SetFont(ABF, FONT_SIZE)
+end
+WatchFrame_OnLoad = JQuest_WatchFrame_OnLoad
+WatchFrame:SetScale(0.9)
+WatchFrameTitle:SetFont(ABF, FONT_SIZE)
