5a58d2f35ca322ff19eda60dbae0bf8a54bb4394
John Cramp <john>
2012-09-14 18:34:30 -0700

    style existing lines; for now

--- code.lua
+++ code.lua
@@ -1,7 +1,9 @@
 local ABF = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\ABF.ttf"
 local FONT_SIZE = 12
 
+--- Style tracker lines.
 -- TODO have this run early enough to get already tracked quests
+-- TODO hooksecurefunc
 local oldWFLTOL = WatchFrameLineTemplate_OnLoad
 local function JQuest_WatchFrameLineTemplate_OnLoad(self)
   --print("WatchFrameLineTemplate_OnLoad")
@@ -12,8 +14,7 @@ local function JQuest_WatchFrameLineTemplate_OnLoad(self)
 end
 WatchFrameLineTemplate_OnLoad = JQuest_WatchFrameLineTemplate_OnLoad
 
--- XXX this needs to be redone if the tracker is ever hidden
--- XXX uh does this happen?
+--- Style tracker title.
 local oldWFOL = WatchFrame_OnLoad
 local function JQuest_WatchFrame_OnLoad(self)
   print("WatchFrame_OnLoad")
@@ -24,3 +25,17 @@ end
 WatchFrame_OnLoad = JQuest_WatchFrame_OnLoad
 WatchFrame:SetScale(0.8)
 WatchFrameTitle:SetFont(ABF, FONT_SIZE)
+
+
+--- Style existing lines.
+-- TODO remove this if I get the above to work for existing buffs
+for i = 1, 50 do
+  line = _G["WatchFrameLine"..i]
+  if line then
+    line.text:SetFont(ABF, FONT_SIZE)
+    line.dash:SetFont(ABF, FONT_SIZE)
+  else
+    break
+  end
+end
+WatchFrame_Update()
