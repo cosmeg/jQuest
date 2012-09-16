9b1af19783d1dd83722805b810c76ebed6ca6864
John Cramp <john>
2012-09-15 22:13:56 -0700

    hooksecurefunc

--- code.lua
+++ code.lua
@@ -1,34 +1,28 @@
 local ABF = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\ABF.ttf"
 local FONT_SIZE = 12
+local TITLE_FONT_SIZE = 16
 
 --- Style tracker lines.
 -- TODO have this run early enough to get already tracked quests
--- TODO hooksecurefunc
-local oldWFLTOL = WatchFrameLineTemplate_OnLoad
-local function JQuest_WatchFrameLineTemplate_OnLoad(self)
+hooksecurefunc("WatchFrameLineTemplate_OnLoad", function(self, ...)
   --print("WatchFrameLineTemplate_OnLoad")
-  oldWFLTOL(self)
   -- TODO tighten up spacing?
   self.text:SetFont(ABF, FONT_SIZE)
   self.dash:SetFont(ABF, FONT_SIZE)
-end
-WatchFrameLineTemplate_OnLoad = JQuest_WatchFrameLineTemplate_OnLoad
+end)
 
 --- Style tracker title.
-local oldWFOL = WatchFrame_OnLoad
-local function JQuest_WatchFrame_OnLoad(self)
+hooksecurefunc("WatchFrame_OnLoad", function(self, ...)
   print("WatchFrame_OnLoad")
-  oldWFOL(self)
   self:SetScale(0.8)
-  WatchFrameTitle:SetFont(ABF, FONT_SIZE)
-end
-WatchFrame_OnLoad = JQuest_WatchFrame_OnLoad
-WatchFrame:SetScale(0.8)
-WatchFrameTitle:SetFont(ABF, FONT_SIZE)
+  WatchFrameTitle:SetFont(ABF, TITLE_FONT_LINE)
+end)
 
 
 --- Style existing lines.
--- TODO remove this if I get the above to work for existing buffs
+-- TODO remove this if I get the above to load early enough
+WatchFrame:SetScale(0.8)
+WatchFrameTitle:SetFont(ABF, TITLE_FONT_LINE)
 for i = 1, 50 do
   line = _G["WatchFrameLine"..i]
   if line then
