982d5a61e3f09162fa5e2f8dec85767bc58015cc
John Cramp <john>
2012-10-05 11:30:26 -0700

    change font

--- code.lua
+++ code.lua
@@ -1,6 +1,7 @@
-local FONT = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\HelveticaNeueBold.ttf"
+local FONT = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\HelveticaNeue.ttf"
 local FONT_SIZE = 12
-local TITLE_FONT_SIZE = 16
+local TITLE_FONT = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\HelveticaNeueBold.ttf"
+local TITLE_FONT_SIZE = 14
 
 --- Style tracker lines.
 -- TODO have this run early enough to get already tracked quests
@@ -15,14 +16,14 @@ end)
 hooksecurefunc("WatchFrame_OnLoad", function(self, ...)
   print("WatchFrame_OnLoad")
   self:SetScale(0.8)
-  WatchFrameTitle:SetFont(FONT, TITLE_FONT_SIZE)
+  WatchFrameTitle:SetFont(TITLE_FONT, TITLE_FONT_SIZE)
 end)
 
 
 --- Style existing lines.
 -- TODO remove this if I get the above to load early enough
 WatchFrame:SetScale(0.8)
-WatchFrameTitle:SetFont(FONT, TITLE_FONT_SIZE)
+WatchFrameTitle:SetFont(TITLE_FONT, TITLE_FONT_SIZE)
 for i = 1, 50 do
   line = _G["WatchFrameLine"..i]
   if line then
