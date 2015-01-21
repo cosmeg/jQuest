5424a1b41af3f413938aaa560c90faaf3dd363bf
John Cramp <jbc.git@gmail.com>
2015-01-20 21:43:56 -0800

    wod fixes

--- code.lua
+++ code.lua
@@ -3,34 +3,33 @@ local FONT_SIZE = 12
 local TITLE_FONT = "Interface\\Addons\\SharedMedia_MyMedia\\fonts\\HelveticaNeueBold.ttf"
 local TITLE_FONT_SIZE = 14
 
---- Style tracker lines.
--- TODO have this run early enough to get already tracked quests
-hooksecurefunc("WatchFrameLineTemplate_OnLoad", function(self, ...)
-  --print("WatchFrameLineTemplate_OnLoad")
-  -- TODO tighten up spacing?
-  self.text:SetFont(FONT, FONT_SIZE)
-  self.dash:SetFont(FONT, FONT_SIZE)
-end)
-
---- Style tracker title.
-hooksecurefunc("WatchFrame_OnLoad", function(self, ...)
-  print("WatchFrame_OnLoad")
-  self:SetScale(0.8)
-  WatchFrameTitle:SetFont(TITLE_FONT, TITLE_FONT_SIZE)
-end)
-
-
---- Style existing lines.
--- TODO remove this if I get the above to load early enough
-WatchFrame:SetScale(0.8)
-WatchFrameTitle:SetFont(TITLE_FONT, TITLE_FONT_SIZE)
-for i = 1, 50 do
-  line = _G["WatchFrameLine"..i]
-  if line then
-    line.text:SetFont(FONT, FONT_SIZE)
-    line.dash:SetFont(FONT, FONT_SIZE)
-  else
-    break
-  end
+local function styleBlock(block)
+  --print(block.HeaderText:GetText())
+  block.HeaderText:SetFont(FONT, FONT_SIZE)
+  block.currentLine.Text:SetFont(FONT, FONT_SIZE)
 end
-WatchFrame_Update()
+
+
+local function styleTracker()
+  QUEST_TRACKER_MODULE.Header.Text:SetFont(TITLE_FONT, TITLE_FONT_SIZE)
+  ObjectiveTrackerFrame:SetScale(0.8)
+  QUEST_TRACKER_MODULE.Header.Background:Hide()
+end
+
+
+hooksecurefunc("ObjectiveTracker_AddBlock", function(block, ...)
+  styleBlock(block)
+end)
+
+
+hooksecurefunc("ObjectiveTracker_OnLoad", function(self, ...)
+  styleTracker()
+end)
+
+
+styleTracker()
+local block = QUEST_TRACKER_MODULE.firstBlock
+while (block) do
+  styleBlock(block)
+  block = block.nextBlock
+end
